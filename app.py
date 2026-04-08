e16d7dee6b687f4713778f995e1f82a7995e9238
Giuliana <medeghinigiuliana@ad-astrainc.com>
2026-04-08 16:55:06 -0400

    first version

---
+++ app.py
@@ -0,0 +1,26 @@
+ 
+from flask import Flask, render_template, request
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("test.html")
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    email = request.form["email"]
+    answer = request.form["answer"]
+
+    conn = sqlite3.connect("db.db")
+    c = conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS results (email TEXT, answer TEXT)")
+    c.execute("INSERT INTO results VALUES (?,?)", (email, answer))
+    conn.commit()
+    conn.close()
+
+    return "Submitted!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
