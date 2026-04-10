b6ecce485eeff8aa6035c3ad97b42cda9231cfe1
Giuliana Medeghini <medeghinigiulian@gmail.com>
2026-04-10 17:42:47 -0400

    add column route

--- app.py
+++ app.py
@@ -331,3 +331,22 @@ def result_detail(id):
     conn.close()
 
     return render_template("result.html", r=result)
+@app.route("/add-new-columns")
+def add_columns():
+    conn = sqlite3.connect("db.db")
+    c = conn.cursor()
+
+    try:
+        c.execute("ALTER TABLE results ADD COLUMN editing_score TEXT")
+    except:
+        pass
+
+    try:
+        c.execute("ALTER TABLE results ADD COLUMN post_edit_score TEXT")
+    except:
+        pass
+
+    conn.commit()
+    conn.close()
+
+    return "Columns added"
